402cb6745434a8dcd0c84c6e0647809d381f675b
sylvain <asylvain10@cegetel.net>
2017-08-15 18:57:37 +0200

    rendering GeoJSON

--- main.js
+++ main.js
@@ -1,31 +1,19 @@
 import 'ol/ol.css';
+import GeoJSON from 'ol/format/geojson';
 import Map from 'ol/map';
-import View from 'ol/view';
-import TileLayer from 'ol/layer/tile';
-import XYZSource from 'ol/source/xyz';
-import proj from 'ol/proj';
-
-//import for adding a vector layer
 import VectorLayer from 'ol/layer/vector';
 import VectorSource from 'ol/source/vector';
-import Feature from 'ol/feature';
-import Point from 'ol/geom/point';
+import View from 'ol/view';
 
-//Styling the point
-import Style from 'ol/style/style';
-import IconStyle from 'ol/style/icon';
-
-//Popup
-import Overlay from 'ol/overlay';
-import coordinate from 'ol/coordinate';
-
+import sync from 'ol-hashed';
 
 const map = new Map({
     target: 'map-container',
     layers: [
-        new TileLayer({
-            source: new XYZSource({
-                url: 'http://tile.stamen.com/terrain/{z}/{x}/{y}.jpg'
+        new VectorLayer({
+            source: new VectorSource({
+                format: new GeoJSON(),
+                url: './data/countries.json'
             })
         })
     ],
@@ -35,39 +23,5 @@ const map = new Map({
     })
 });
 
-const position = new VectorSource();
-const vector = new VectorLayer({
-    source: position
-});
-
-vector.setStyle(new Style({
-    image: new IconStyle({
-        src: './data/marker.png'
-    })
-}))
-
-map.addLayer(vector);
-
-navigator.geolocation.getCurrentPosition((pos) => {
-    const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
-    map.getView().animate({ center: coords, zoom: 10 });
-    position.addFeature(new Feature(new Point(coords)));
-});
-
-var overlay = new Overlay({
-    element: document.getElementById('popup-container'),
-    positioning: 'bottom-center',
-    offset: [0, -10]
-});
-map.addOverlay(overlay);
-
-map.on('click', (e) => {
-    overlay.setPosition();
-    var features = map.getFeaturesAtPixel(e.pixel);
-    if (features) {
-        var coords = features[0].getGeometry().getCoordinates();
-        var hdms = coordinate.toStringHDMS(proj.toLonLat(coords));
-        overlay.getElement().innerHTML = hdms;
-        overlay.setPosition(coords);
-    }
-});
+//the map stayed where we left it in a reload.
+sync(map);
