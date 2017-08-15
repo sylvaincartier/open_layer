e5b53fbd4f734efd9eeb4df19967ec0e2539f960
sylvain <asylvain10@cegetel.net>
2017-08-15 18:23:11 +0200

    Drawing a point at your location

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ import TileLayer from 'ol/layer/tile';
 import XYZSource from 'ol/source/xyz';
 import proj from 'ol/proj';
 
+//import for adding a vector layer
+import VectorLayer from 'ol/layer/vector';
+import VectorSource from 'ol/source/vector';
+import Feature from 'ol/feature';
+import Point from 'ol/geom/point';
+
+//Styling the point
+import Style from 'ol/style/style';
+import IconStyle from 'ol/style/icon';
+
 
 const map = new Map({
     target: 'map-container',
@@ -21,7 +31,21 @@ const map = new Map({
     })
 });
 
+const position = new VectorSource();
+const vector = new VectorLayer({
+    source: position
+});
+
+vector.setStyle(new Style({
+    image: new IconStyle({
+        src: './data/marker.png'
+    })
+}))
+
+map.addLayer(vector);
+
 navigator.geolocation.getCurrentPosition((pos) => {
     const coords = proj.fromLonLat([pos.coords.longitude, pos.coords.latitude]);
     map.getView().animate({ center: coords, zoom: 10 });
+    position.addFeature(new Feature(new Point(coords)));
 });
