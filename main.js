668a65cc9d9b5b7c853e19959aa851c35f7e7837
sylvain <asylvain10@cegetel.net>
2017-08-17 16:28:38 +0200

    Making it look nice

--- main.js
+++ main.js
@@ -12,6 +12,12 @@ import Modify from 'ol/interaction/modify';
 import Draw from 'ol/interaction/draw';
 import Snap from 'ol/interaction/snap';
 
+import Fill from 'ol/style/fill';
+import Stroke from 'ol/style/stroke';
+import Style from 'ol/style/style';
+import colormap from 'colormap';
+import sphere from 'ol/sphere';
+
 const source = new VectorSource();
 
 const map = new Map({
@@ -22,10 +28,10 @@ const map = new Map({
     })
 });
 
-const layer = new VectorLayer({
-    source: source
-});
-map.addLayer(layer);
+// const layer = new VectorLayer({
+//     source: source,
+// });
+// map.addLayer(layer);
 
 map.addInteraction(new DragDrop({
     source: source,
@@ -57,6 +63,41 @@ source.on('change', function() {
     const json = format.writeFeatures(features);
     download.href = 'data:text/json;charset=utf-8,' + json;
 });
+
+const min = 1e8; //the smallest area
+const max = 2e13; //the biggest area
+const steps = 50;
+const ramp = colormap({
+    colormap: 'blackbody',
+    nshades: steps
+})
+
+function clamp(value, low, high) {
+    return Math.max(low, Math.min(value, high));
+}
+
+function getColor(feature) {
+    const area = sphere.getArea(feature.getGeometry());
+    const f = Math.pow(clamp((area - min) / (max - min), 0, 1), 1 / 2);
+    const index = Math.round(f * (steps - 1));
+    return ramp[index];
+}
+
+const layer = new VectorLayer({
+    source: source,
+    style: function(feature) {
+        return new Style({
+            fill: new Fill({
+                color: getColor(feature)
+            }),
+            stroke: new Stroke({
+                color: 'rgba(255,255,255,0.8)'
+            })
+        });
+    }
+});
+
+map.addLayer(layer);
 
 
 //the map stayed where we left it in a reload.
